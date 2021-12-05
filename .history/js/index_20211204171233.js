2cbe60da4860085b3ab68b1f0068170edffec4b7
NgocNB20 <ngocmeu2000@gmail.com>
2021-12-05 08:54:21 +0700

    Initial commit

---
+++ .history/js/index_20211204171233.js
@@ -0,0 +1,14 @@
+$('#slide3d .owl-carousel').owlCarousel({
+    items: 1,
+    lazyLoad: true,
+    loop: true,
+    margin: 0,
+    nav: true,
+});
+$('#owl-blogs .owl-carousel').owlCarousel({
+    items: 1,
+    lazyLoad: true,
+    loop: true,
+    margin: 10,
+    nav: true,
+});
